80e9e48a65be8009d59f2c8565793de191058f62
Dave Turnbull <dave@redcordial.org>
2010-04-13 15:17:10 +1000

    the linux version needs -luuid specifically

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,6 +4,10 @@ RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 extension_name = 'sysuuid'
 dir_config(extension_name)
 
+if `uname`.strip == 'Linux'
+    have_library('uuid')
+end
+
 $CFLAGS += ' -Werror -Wall'
 
 create_makefile(extension_name)
