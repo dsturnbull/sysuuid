068e03069e1d6a7a827d88577f8a5a790a4340ea
Dave Turnbull <dave@redcordial.org>
2011-05-19 15:51:50 +1000

    SunOS needs libuuid too

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,8 +4,8 @@ RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 extension_name = 'sysuuid'
 dir_config(extension_name)
 
-if `uname`.strip == 'Linux'
-    have_library('uuid')
+if ['Linux', 'SunOS'].include?(`uname`.strip)
+  have_library('uuid')
 end
 
 $CFLAGS += ' -Werror -Wall'
